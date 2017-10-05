d7eaf90554ac58f259014f0b88436ed2af99a415
Maciej Nowak <maciej.nowak90@gmail.com>
2017-10-05 14:31:19 +0200

    Add sass middleware

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 
+const path = require('path');
+const sassMiddleware = require('node-sass-middleware');
+
 app.set('view engine', 'pug');
+
+app.use(sassMiddleware({
+    /* Options */
+    src: __dirname,
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/prefix'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}));
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
   res.render('chat')
