bcb64e858090b74141d3b5881314d42a46106ebf
Maciej Nowak <maciej.nowak90@gmail.com>
2017-10-05 14:08:17 +0200

    Init socket.io and express.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res){
+  res.render('chat')
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
